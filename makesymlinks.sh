8f0158bb052564878bc894c761c53c869c7056cd
serdotlinecho <serdotlinecho@gmail.com>
2013-06-12 15:31:55 +0800

    Added i3 config in symlink list

--- makesymlinks.sh
+++ makesymlinks.sh
@@ -8,7 +8,7 @@
 
 dir=~/dotfiles                    # dotfiles directory
 olddir=~/dotfiles_old             # old dotfiles backup directory
-files="vimrc zshrc tmux.conf Xdefaults xinitrc"     # list of files/folders to symlink in homedir
+files="vimrc zshrc tmux.conf Xdefaults xinitrc i3 i3status.conf"     # list of files/folders to symlink in homedir
 
 ##########
 
