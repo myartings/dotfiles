fde577d6cb64feb5164eab07d6f4d3f6d1a1cc85
serdotlinecho <serdotlinecho@gmail.com>
2013-06-18 12:50:47 +0800

    Added Xresources

--- makesymlinks.sh
+++ makesymlinks.sh
@@ -8,7 +8,7 @@
 
 dir=~/dotfiles                    # dotfiles directory
 olddir=~/dotfiles_old             # old dotfiles backup directory
-files="vimrc zshrc tmux.conf Xdefaults xinitrc i3 i3status.conf weechat"     # list of files/folders to symlink in homedir
+files="vimrc zshrc tmux.conf Xresources xinitrc i3 i3status.conf weechat"     # list of files/folders to symlink in homedir
 
 ##########
 
