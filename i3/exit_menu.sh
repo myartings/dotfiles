229aa01a9e269c2a7814024785c52dca57f5d347
serdotlinecho <serdotlinecho@gmail.com>
2013-08-22 16:15:27 +0800

    Changed color and font

--- i3/exit_menu.sh
+++ i3/exit_menu.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 while [ "$select" != "YES" -a "$select" != "NO" ]; do
-    select=$(echo -e 'YES\nNO' | dmenu -y 250 -nb '#2b2b2b' -nf '#bfb5b5' -sb '#f00060' -sf '#000000' -fn 'Droid Sans Mono Dotted-12:normal' -i -p "Do you really want to exit i3? This will end your X session.")
+    select=$(echo -e 'YES\nNO' | dmenu -y 250 -nb '#2b2b2b' -nf '#bfb5b5' -sb '#dc322f' -sf '#000000' -fn 'Droid Sans Mono Dotted-12:normal' -i -p "Do you really want to exit i3? This will end your X session.")
     [ -z "$select" ] && exit 0
 done
 [ "$select" = "NO" ] && exit 0
