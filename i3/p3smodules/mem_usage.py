157fabe9140c91607f62904e23f5a3c1d5d8bd39
serdotlinecho <serdotlinecho@gmail.com>
2013-09-04 00:47:24 +0800

    First commit

---
+++ i3/p3smodules/mem_usage.py
@@ -0,0 +1,27 @@
+import psutil
+from time import time
+
+class Py3status:
+    def RAMinfo(self, json, i3status_config):
+        
+        response = {'full_text' : '', 'name' : 'RAMinfo'}
+
+        mem_total = int(psutil.virtual_memory().total / 1048576)
+        mem_used = int(psutil.virtual_memory().used / 1048576)
+        mem_buffers = int(psutil.virtual_memory().buffers / 1048576)
+        mem_cached = int(psutil.virtual_memory().cached / 1048576)
+        mem_real_used = mem_used - mem_buffers - mem_cached
+        mem_percent = int(psutil.virtual_memory().percent)
+
+        if mem_percent <= 50:
+            response['color'] = i3status_config['color_good']
+        elif mem_percent <= 75:
+            response['color'] = i3status_config['color_degraded']
+        else:
+            response['color'] = i3status_config['color_bad']
+
+        response['full_text'] = 'Î {}MB/{}MB'.format(mem_real_used, mem_total)
+
+        response['cached_until'] = time() + 5
+
+        return(-5, response)
