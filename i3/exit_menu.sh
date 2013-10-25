7c03ce2aa3a9cd3793cf99baa9e81c669e0e21d3
serdotlinecho <serdotlinecho@gmail.com>
2013-10-26 00:46:45 +0800

    Config changes

--- i3/exit_menu.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-# source: https://github.com/oblique/configs/blob/master/.i3/bin/exit_menu.sh
-
-while [ "$select" != "YES" -a "$select" != "NO" ]; do
-    select=$(echo -e 'YES\nNO' | dmenu -y 250 -nb '#161616' -nf '#bfb5b5' -sb '#dc322f' -sf '#000000' -fn 'Droid Sans Mono Dotted-12:normal' -i -p "Do you really want to exit i3? This will end your X session.")
-    [ -z "$select" ] && exit 0
-done
-[ "$select" = "NO" ] && exit 0
-i3-msg exit
