3585cea8ab1c91da117a458cd6ad512fe9b2e6c3
serdotlinecho <serdotlinecho@gmail.com>
2013-10-29 23:34:13 +0800

    Last commit

--- zsh/notifyosd.zsh
+++ zsh/notifyosd.zsh
@@ -1,9 +1,9 @@
 # commands to ignore
-cmdignore=(vim top htop)
+cmdignore=(vim v vimdiff vless ping pyhp git g gd less w3m feh mutt man top htop watch newsbeuter ranger alsamixer almi ncmpcpp ncm mplayer mpl mpv powertop cfg-{tmuxrc,zshrc,alias,function,vimrc,vimp,xresources,xinitrc,i3conf,i3status,newsbeuter,mplayer} ytv yts-{gotbletu,hawkkeydavischannel,jbcast,linux4unme,metalx1000,rt,rtamerica,whygoldnsilver,valve,gregvegas,ohheyitslou} )
 
 # end and compare timer, notify-send if needed
 function notifyosd-precmd() {
-	retval=$?
+    retval=$?
     if [[ ${cmdignore[(r)$cmd_basename]} == $cmd_basename ]]; then
         return
     else
@@ -12,8 +12,8 @@ function notifyosd-precmd() {
             ((cmd_time=$cmd_end - $cmd_start))
         fi
         if [ $retval -gt 0 ]; then
-			cmdstat="with warning"
-		else
+            cmdstat="with warning"
+        else
             cmdstat="successfully"
         fi
         if [ ! -z "$cmd" -a $cmd_time -gt 10 ]; then
@@ -33,7 +33,7 @@ precmd_functions+=( notifyosd-precmd )
 # get command name and start the timer
 function notifyosd-preexec() {
     cmd=$1
-    cmd_basename=${${cmd:s/sudo //}[(ws: :)1]} 
+    cmd_basename=${${cmd:s/sudo //}[(ws: :)1]}
     cmd_start=`date +%s`
 }
 
